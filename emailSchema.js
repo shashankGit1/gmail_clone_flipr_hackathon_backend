2efb06ba52d28a55b6c91a0a0b06ffa97ff84a51
cawPratyush <pratyush.shivam@cawstudios.com>
2021-06-26 21:15:57 +0530

    fxing

---
+++ emailSchema.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose'
+
+// defining schema 
+const mayadataSchema = mongoose.Schema({
+    to: String,
+    from: String,
+    subject: String,
+    text: String,
+    endTime: String,
+})
+
+export default mongoose.model('emailContent', mayadataSchema)
--- index.js
+++ index.js
@@ -6,27 +6,67 @@ const API_KEY = 'SG.F204PKfpTsqn2od8KQNBZQ.6HVKkT-KEkQgPJWrOwoh8EGmHGSAANaxWPh_5
 import sgMail from '@sendgrid/mail'
 import cors from 'cors'
 import bodyParser from 'body-parser'
+import emailSchemaDB from './emailSchema.js'
 app.use(cors())
 let data = {}
+// app.use(express.json())
+
+const connection_url = 'mongodb+srv://admin:caKGhdd4B9PDSBWx@cluster0.4yxul.mongodb.net/hackathondb?retryWrites=true&w=majority'
+
+mongoose.connect(connection_url, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+const db = mongoose.connection
+db.once('open', () => {
+    console.log("Database connected!")
+    const dataCollection = db.collection('emailContents')
+    const changeStream = dataCollection.watch();
+    changeStream.on('change', (change) => {
+        console.log("A change occured", change)
+
+    })
+})
+
+
+
 
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.post('/api/v1/data', (req, res) => {
     console.log(req.body)
-    res.status(200).send({
-        id: 3443,
-        message: 'success'
-    })
+    const dbMessage = req.body
 
-    sgMail.send(req.body).then((res) => {
-        console.log("email send")
-    }).catch((err) => {
-        console.log(err.message)
+    // sgMail.send(req.body).then((res) => {
+    //     console.log("email send")
+    // }).catch((err) => {
+    //     console.log(err.message)
+    // })
+    emailSchemaDB.create(dbMessage, (err, data) => {
+
+        if (err) {
+            res.status(500).send(err) // internal server errpr
+        }
+        else {
+            res.status(201).send(data)
+        }
     })
 })
 
 
+app.get("/api/v1/emails", (req, res) => {
+    emailSchemaDB.find((err, data) => {
+        if (err) {
+            res.status(500).send(err)
+        }
+        else {
+            res.status(200).send(data) // for get / downloading data we set 200 status code and it represents OKAY
+        }
+    })
+})
 
 
 
@@ -44,3 +84,12 @@ const message = {
 app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`)
 })
+
+
+
+
+
+
+
+// Database Password : caKGhdd4B9PDSBWx
+// Database: mongodb + srv://admin:<password>@cluster0.4yxul.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
